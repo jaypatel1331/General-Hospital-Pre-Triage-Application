132cd131f5dd839bb313fcd89fe2fd5e449a950b
jaypatel1331 <82784261+jaypatel1331@users.noreply.github.com>
2021-11-05 09:09:24 -0700

    Update Ticket.h

--- Ticket.h
+++ Ticket.h
@@ -1,14 +1,3 @@
-/*********************************************************
- * Name: Jay Ashishbhai Patel
- * Student ID: 154925192
- * Seneca email: japatel31@myseneca.ca
- * Section: OOP 244 NGG
- * Project : Milestone 3
- *
- * I have done all the coding by myself and only copied the code
- *  that my professor provided to complete my workshops and assignments.
- **********************************************************/
-
 #ifndef SDDS_TICKET_H_
 #define SDDS_TICKET_H_
 #include "Time.h"
