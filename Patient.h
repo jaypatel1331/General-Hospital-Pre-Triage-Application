4a0cc90362a4c2c50743f32caa13fd71c9f6766a
jaypatel1331 <82784261+jaypatel1331@users.noreply.github.com>
2021-11-05 09:08:29 -0700

    Update Patient.h

--- Patient.h
+++ Patient.h
@@ -1,14 +1,3 @@
-/*********************************************************
- * Name: Jay Ashishbhai Patel
- * Student ID: 154925192
- * Seneca email: japatel31@myseneca.ca
- * Section: OOP 244 NGG
- * Project : Milestone 3
- *
- * I have done all the coding by myself and only copied the code
- *  that my professor provided to complete my workshops and assignments.
- **********************************************************/
-
 #ifndef SDDS_PATIENT_H
 #define SDDS_PATIENT_H
 #include "IOAble.h"
